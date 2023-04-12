5ea41869a49f78abbdf2c19923fd7c5e83c6f8c3
Hewerton Adão <hewerton80@gmail.com>
2023-04-12 20:28:06 -0300

    update version 0.2.1

--- rollup.config.js
+++ rollup.config.js
@@ -12,84 +12,96 @@ import dts from "rollup-plugin-dts";
 import pkg from "./package.json";
 import { getFiles } from "./scripts/buildUtils";
 
-function getComponentsFilesMap(path) {
+function getComponentsFilesMap(path, ext) {
   console.log("path", path);
   const componentsFilesMap = {};
-  getFiles(path, ["index.tsx"]).forEach((file) => {
+  getFiles(path, ext).forEach((file) => {
     componentsFilesMap[file.match(/[A-Z][a-z]+/g)[1]] = file;
   });
   return componentsFilesMap;
 }
+function gethHooksFilesMap(path) {
+  console.log("path", path);
+  const componentsFilesMap = {};
+  getFiles(path, [".d.ts"]).forEach((file) => {
+    componentsFilesMap[`use${file.match(file.match(/[A-Z][a-z]+/g))[0]}`] =
+      file;
+  });
+  return componentsFilesMap;
+}
+const basePath = "dist/esm/types";
 
 export default [
-  // {
-  //   // input: [
-  //   //   "src/index.ts",
-  //   //   // ...getFiles("./src/Components", extensions),
-  //   //   // ...getFiles("./src/hooks", extensions),
-  //   //   // ...getFiles("./src/utils", extensions),
-  //   // ],
-  //   input: {
-  //     Button: "src/Components/ui/forms/Button/index.tsx",
-  //   },
-  //   output: {
-  //     dir: "./dist",
-  //     format: "esm",
-  //     sourcemap: true,
-  //     exports: "named",
-  //     inlineDynamicImports: false,
-  //   },
-  //   treeshake: {
-  //     moduleSideEffects: false,
-  //   },
-  //   // input: ["src/index.ts"],
-  //   // output: [
-  //   //   // {
-  //   //   //   file: pkg.main,
-  //   //   //   format: "cjs",
-  //   //   //   exports: "named",
-  //   //   //   sourcemap: true,
-  //   //   // },
-  //   //   {
-  //   //     file: pkg.module,
-  //   //     format: "esm",
-  //   //     exports: "named",
-  //   //     sourcemap: true,
-  //   //   },
-  //   // ],
-  //   plugins: [
-  //     external(),
-  //     postcss({ modules: true }),
-  //     image(),
-  //     svgr(),
-  //     resolve(),
-  //     typescript({
-  //       tsconfig: "./tsconfig.build.json",
-  //       declaration: true,
-  //       declarationDir: "dist",
-  //     }),
-  //     commonjs(),
-  //     terser(),
-  //     json(),
-  //     // dts(),
-  //   ],
-  // },
   {
-    input: {
-      Checkbox: "src/Components/ui/forms/Checkbox/index.tsx",
-      Switch: "src/Components/ui/forms/Switch/index.tsx",
-      Button: "src/Components/ui/forms/Button/index.tsx",
-      // ...getComponentsFilesMap("src/Components/ui"),
-    },
-    output: {
-      dir: "./dist",
-      format: "esm",
-      sourcemap: true,
-      exports: "named",
-      inlineDynamicImports: false,
-    },
-    // input: "dist/esm/types/index.d.ts",
-    // output: [{ file: "dist/index.d.ts", format: "esm" }],
+    // input: {
+    //   ...getComponentsFilesMap("src/Components/ui", ["index.tsx"]),
+    // },
+    //   input: {
+    //     Button: "src/Components/ui/forms/Button/index.tsx",
+    //   },
+    // output: {
+    //   dir: "./dist",
+    //   format: "esm",
+    //   sourcemap: true,
+    //   exports: "named",
+    //   inlineDynamicImports: false,
+    // },
+    //   treeshake: {
+    //     moduleSideEffects: false,
+    //   },
+    input: ["src/index.ts"],
+    output: [
+      // {
+      //   file: pkg.main,
+      //   format: "cjs",
+      //   exports: "named",
+      //   sourcemap: true,
+      // },
+      {
+        file: pkg.module,
+        format: "esm",
+        exports: "named",
+        sourcemap: true,
+      },
+    ],
+    plugins: [
+      external(),
+      postcss({ modules: true }),
+      image(),
+      svgr(),
+      resolve(),
+      typescript({
+        tsconfig: "./tsconfig.build.json",
+        // declaration: true,
+        // declarationDir: "dist",
+      }),
+      commonjs(),
+      terser(),
+      json(),
+      // dts(),
+    ],
+  },
+  {
+    // input: {
+    //   // Checkbox: "src/Components/ui/forms/Checkbox/index.tsx",
+    //   // Switch: "src/Components/ui/forms/Switch/index.tsx",
+    //   // Button: "src/Components/ui/forms/Button/index.tsx",
+    //   ...getComponentsFilesMap(`${basePath}/Components/ui`, ["index.d.ts"]),
+    //   ...gethHooksFilesMap(`${basePath}/hooks`),
+    //   HikariProviders: `${basePath}/context/index.d.ts`,
+    //   theme: `${basePath}/styles/theme.d.ts`,
+    //   // theme: "src/styles/theme.ts",
+    //   // ...getComponentsFilesMap("src/context", ["index.tsx"]),
+    // },
+    // output: {
+    //   dir: "./dist",
+    //   format: "esm",
+    //   sourcemap: true,
+    //   exports: "named",
+    //   inlineDynamicImports: false,
+    // },
+    input: "dist/esm/types/index.d.ts",
+    output: [{ file: "dist/index.d.ts", format: "esm" }],
     external: [/\.css$/],
     plugins: [dts.default()],
   },
--- test-run.script.js
+++ test-run.script.js
@@ -1,10 +1,11 @@
 const { getFiles } = require("./scripts/buildUtils");
-const files = getFiles("dist/esm/types/src/Components/ui", ["index.d.ts"]);
+const files = getFiles("src/hooks", [".ts"]);
 console.log("files");
 
 let object = {};
 
 files.forEach((file) => {
-  object[file.match(/[A-Z][a-z]+/g)[1]] = file;
+  // console.log(file.match(/[A-Z][a-z]+/g));
+  object[`use${file.match(file.match(/[A-Z][a-z]+/g))[0]}`] = file;
 });
 console.log("object", object);
